34480bc42489a4c57a94bd127b9eaa00955d1ca7
jbh6357 <kym3157@naver.com>
2025-08-11 09:35:48 +0900

    Update menu.js

--- WEBAPP/src/api/menu.js
+++ WEBAPP/src/api/menu.js
@@ -2,10 +2,10 @@ import { API_CONFIG } from "../config/api.js";
 
 /**
  * 사용자의 메뉴 목록을 가져오는 API
- * @param {string} userId - 사용자 ID (이메일)
+ * @param {int} userId - 사용자 ID (이메일)
  * @returns {Promise<Array>} 메뉴 아이템 배열
  */
-export const getMenusByUserId = async (userId, language) => {
+export const getMenusByUserId = async (userId) => {
   try {
     const response = await fetch(
       `${API_CONFIG.BASE_URL}/menus/${encodeURIComponent(userId)}`,
